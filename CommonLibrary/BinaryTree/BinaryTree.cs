060f2dbe86ce4f8213b6c2f4c6ae229a160e7dcb
DUSTIN-LENOVO\dtpal <dtpaltz@gmail.com>
2022-07-21 06:44:31 -0700

    Adding binary tree stuff

---
+++ CommonLibrary/BinaryTree/BinaryTree.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.BinaryTree
+{
+    public class BinaryTree
+    {
+        public TreeNode Root { get; set; }
+
+        public bool Add(int value)
+        {
+            TreeNode before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data) //Is new node in left tree? 
+                    after = after.LeftNode;
+                else if (value > after.Data) //Is new node in right tree?
+                    after = after.RightNode;
+                else
+                {
+                    //Exist same value
+                    return false;
+                }
+            }
+
+            TreeNode newNode = new TreeNode();
+            newNode.Data = value;
+
+            if (this.Root == null)//Tree ise empty
+                this.Root = newNode;
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+
+            return true;
+        }
+
+        public TreeNode Find(int value)
+        {
+            return this.Find(value, this.Root);
+        }
+
+        public void Remove(int value)
+        {
+            this.Root = Remove(this.Root, value);
+        }
+
+        private TreeNode Remove(TreeNode parent, int key)
+        {
+            if (parent == null) return parent;
+
+            if (key < parent.Data) parent.LeftNode = Remove(parent.LeftNode, key);
+            else if (key > parent.Data)
+                parent.RightNode = Remove(parent.RightNode, key);
+
+            // if value is same as parent's value, then this is the node to be deleted  
+            else
+            {
+                // node with only one child or no child  
+                if (parent.LeftNode == null)
+                    return parent.RightNode;
+                else if (parent.RightNode == null)
+                    return parent.LeftNode;
+
+                // node with two children: Get the inorder successor (smallest in the right subtree)  
+                parent.Data = MinValue(parent.RightNode);
+
+                // Delete the inorder successor  
+                parent.RightNode = Remove(parent.RightNode, parent.Data);
+            }
+
+            return parent;
+        }
+
+        private int MinValue(TreeNode node)
+        {
+            int minv = node.Data;
+
+            while (node.LeftNode != null)
+            {
+                minv = node.LeftNode.Data;
+                node = node.LeftNode;
+            }
+
+            return minv;
+        }
+
+        private TreeNode Find(int value, TreeNode parent)
+        {
+            if (parent != null)
+            {
+                if (value == parent.Data) return parent;
+                if (value < parent.Data)
+                    return Find(value, parent.LeftNode);
+                else
+                    return Find(value, parent.RightNode);
+            }
+
+            return null;
+        }
+
+        public int GetTreeDepth()
+        {
+            return this.GetTreeDepth(this.Root);
+        }
+
+        private int GetTreeDepth(TreeNode parent)
+        {
+            return parent == null ? 0 : Math.Max(GetTreeDepth(parent.LeftNode), GetTreeDepth(parent.RightNode)) + 1;
+        }
+
+        public void TraversePreOrder(TreeNode parent)
+        {
+            if (parent != null)
+            {
+                Debug.Write(parent.Data + " ");
+                TraversePreOrder(parent.LeftNode);
+                TraversePreOrder(parent.RightNode);
+            }
+        }
+
+        public void TraverseInOrder(TreeNode parent)
+        {
+            if (parent != null)
+            {
+                TraverseInOrder(parent.LeftNode);
+                Debug.Write(parent.Data + " ");
+                TraverseInOrder(parent.RightNode);
+            }
+        }
+
+        public void TraversePostOrder(TreeNode parent)
+        {
+            if (parent != null)
+            {
+                TraversePostOrder(parent.LeftNode);
+                TraversePostOrder(parent.RightNode);
+                Debug.Write(parent.Data + " ");
+            }
+        }
+    }
+}
---
+++ CommonLibrary/BinaryTree/TreeNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommonLibrary.BinaryTree
+{
+	public class TreeNode
+	{
+		public TreeNode LeftNode { get; set; }
+		public TreeNode RightNode { get; set; }
+		public int Data { get; set; }
+	}
+}

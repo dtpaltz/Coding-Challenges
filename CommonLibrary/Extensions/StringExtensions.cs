bd49eb3fd1ab27d7ccd7c21f535240252906db6a
DUSTIN-LENOVO\dtpal <dtpaltz@gmail.com>
2022-07-22 11:15:04 -0700

    Adding string extensions

---
+++ CommonLibrary/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace CommonLibrary.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Determine if given two strings are anagrams of each other or not. An anagram of a string is another string that contains the same characters, but the order of characters can be different.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns>Return true if given two strings are anagrams of each other, otherwise false.</returns>
+        public static bool AreAnagrams(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2) || s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            char[] a1 = s1.ToLower().ToCharArray();
+            char[] a2 = s2.ToLower().ToCharArray();
+
+            Array.Sort(a1);
+            Array.Sort(a2);
+
+            string res1 = new string(a1);
+            string res2 = new string(a2);
+
+            return res1 == res2;
+        }
+    }
+}

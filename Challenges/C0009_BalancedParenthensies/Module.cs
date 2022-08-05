8d311986d7a2f76d67971ee573a63bceb52f6e6a
DUSTIN-LENOVO\dtpal <dtpaltz@gmail.com>
2022-08-05 13:42:08 -0700

    BalancedParenthensies

---
+++ Challenges/C0009_BalancedParenthensies/Module.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace Challenges.C0009_BalancedParenthensies
+{
+    internal class Module
+    {
+        public static bool balancedParenthensies(String s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == '[' || c == '(' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+                else if (c == ')')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+                else if (c == '}')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '{')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Challenges/C0009_BalancedParenthensies/UnitTests.cs
@@ -0,0 +1,19 @@
+using CommonLibrary.Grid;
+using NUnit.Framework;
+
+namespace Challenges.C0009_BalancedParenthensies
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var m = new Module();
+        }
+    }
+}

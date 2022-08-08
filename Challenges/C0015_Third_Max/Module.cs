1b5162b8fb1be9d6c6335f04b47896a8d41d6f12
DUSTIN-LENOVO\dtpal <dtpaltz@gmail.com>
2022-08-08 16:56:17 -0700

    cleaning up third max

--- Challenges/C0015_Third_Max/Module.cs
+++ Challenges/C0015_Third_Max/Module.cs
@@ -22,8 +22,8 @@ namespace Challenges.C0015_Third_Max
                 }
             }
 
-            // Find first largest element
-            int second = -1 * first;
+            // Find second largest element
+            int second = -first;
             for (int i = 0; i < nums.Length; i++)
             {
                 int n = nums[i];
@@ -33,8 +33,8 @@ namespace Challenges.C0015_Third_Max
                 }
             }
 
-            // Find first largest element
-            int third = -1 * second;
+            // Find third largest element
+            int third = -second;
             for (int i = 0; i < nums.Length; i++)
             {
                 int n = nums[i];
